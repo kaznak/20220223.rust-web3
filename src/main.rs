13f3cd56a7a427b93df892fc83213f25d1f01911
kaznak <kaznak.at.work@gmail.com>
2022-02-23 13:10:50 +0900

    counter

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,18 @@
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 
+use std::sync::Mutex;
+
+struct AppStateWithCounter {
+    counter: Mutex<i32>, // <- Mutex is necessary to mutate safely across threads
+}
+
+async fn index(data: web::Data<AppStateWithCounter>) -> String {
+    let mut counter = data.counter.lock().unwrap(); // <- get counter's MutexGuard
+    *counter += 1; // <- access counter inside MutexGuard
+
+    format!("Request number: {}", counter) // <- response with count
+}
+
 #[get("/")]
 async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hello actix-web!")
@@ -16,11 +29,20 @@ async fn manual_hello() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    let counter = web::Data::new(AppStateWithCounter {
+        counter: Mutex::new(0),
+    });
+
+    HttpServer::new(move || {
         App::new()
             .service(hello)
             .service(echo)
             .route("/hey", web::get().to(manual_hello))
+            .service(
+                web::scope("/counter")
+                    .app_data(counter.clone())
+                    .route("", web::get().to(index)),
+            )
     })
     .bind("127.0.0.1:8080")?
     .run()
