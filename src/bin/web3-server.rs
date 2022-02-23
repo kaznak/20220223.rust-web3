ee75fd09fe296f77340e9d90f22d75ef83004228
kaznak <kaznak.at.work@gmail.com>
2022-02-24 02:37:23 +0900

    update

--- src/bin/web3-server.rs
+++ src/bin/web3-server.rs
@@ -1,4 +1,4 @@
-use actix_web::{web, App, HttpServer};
+use actix_web::{get, web, App, HttpServer};
 use std::sync::Mutex;
 
 const ENDPOINT: &str = "https://mainnet.infura.io/v3/a7f5bdd129cc4688bb70483556e3799f";
@@ -7,21 +7,14 @@ struct AppState {
     web3: Mutex<web3::Web3<web3::transports::Http>>,
 }
 
-async fn balance(data: web::Data<AppState>) -> String {
+#[get("/balance/{account}")]
+async fn balance(web::Path(account): web::Path<String>, data: web::Data<AppState>) -> String {
     let rt = actix_rt::Runtime::new().unwrap();
     let web3 = data.web3.lock().unwrap();
-    eprintln!("Calling accounts.");
-    let mut accounts = rt.block_on(web3.eth().accounts()).unwrap_or(Vec::new());
-    eprintln!("Accounts: {:?}", accounts);
-    accounts.push("00a329c0648769a73afac7f9381e08fb43dbea72".parse().unwrap());
-
-    eprintln!("Calling balance.");
-    let mut ret = String::new();
-    for account in accounts {
-        let balance = rt.block_on(web3.eth().balance(account, None)).unwrap();
-        ret.push_str(&format!("{:?} {}\n", account, balance));
-    }
-    return ret;
+    let balance = rt
+        .block_on(web3.eth().balance(account.parse().unwrap(), None))
+        .unwrap();
+    return format!("{:?} {}\n", account, balance);
 }
 
 #[actix_web::main]
@@ -35,7 +28,7 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(move || {
         App::new()
             .app_data(state.clone())
-            .route("/", web::get().to(balance))
+            .service(balance)
     })
     .workers(1)
     .bind("0.0.0.0:8080")
